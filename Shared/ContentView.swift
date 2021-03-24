026ca3d1cd3b608b9467aa5ef20d8488578d193f
Xinyi Xiang <xinyixiang1129@gmail.com>
2021-03-23 22:26:41 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Vicky Xiang on 3/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/RubikatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RubikatApp.swift
+//  Shared
+//
+//  Created by Vicky Xiang on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct RubikatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
