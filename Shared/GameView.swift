657c514ad137c6334a4117a84c9b483080e2e213
Xinyi Xiang <xinyixiang1129@gmail.com>
2021-04-18 13:41:41 -0500

    Floating blocks (for now)

--- Shared/GameView.swift
+++ Shared/GameView.swift
@@ -14,26 +14,33 @@ struct DraggableRect : View {
     
     var colorsDict = ([[Color]](repeating: [Color.red, Color.blue, Color.yellow, Color.green, Color.orange, Color.purple], count: 4).flatMap{$0} + [Color.clear]).shuffled()
     
+    @State
+    private var location = CGPoint(x:30,y:30)
     @GestureState
-    var position = CGSize.zero
+    var draggedOffset = CGSize.zero
     
     var body : some View {
         RoundedRectangle(cornerRadius: 20.0)
             .fill(colorsDict[rowId * 5 + colId])
+            .position(location)
             .animation(.spring())
-            .offset(x: self.position.width, y: self.position.height)
+            .offset(x: self.draggedOffset.width, y: self.draggedOffset.height)
             .gesture(
-                DragGesture()
-                    .updating($position, body: { value, state, transaction in
-                                        state = value.translation
+                DragGesture().updating($draggedOffset, body: { value, state, transaction in state = value.translation})
+                    .onEnded{ value in
+                        if (abs(self.draggedOffset.width - 60.0) != 0) {
+                            self.location = value.location
+                        }
+                        else if (abs(self.draggedOffset.height - 60.0) != 0) {
+                            self.location = value.location
+                        }
+                        else {
+                        }
                     })
-            )
     }
 }
 
 struct GameView: View {
-    let grid = Grid.create()
-    
     var sideLength: CGFloat = 60.0
     
     enum SwipeHorizontalDirection: String {
@@ -52,6 +59,7 @@ struct GameView: View {
                                 VStack{
                                         DraggableRect(rowId: rowIndex, colId: colIndex)
                                             .frame(width: sideLength, height: sideLength)
+                                            
                                 }
                             }
                         }
