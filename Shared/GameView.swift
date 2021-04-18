05d46a9f37a8386512f6d20f96bd7a80e0613af0
Xinyi Xiang <xinyixiang1129@gmail.com>
2021-04-18 13:23:27 -0500

    Realize individual block movement

--- Shared/GameView.swift
+++ Shared/GameView.swift
@@ -8,30 +8,33 @@
 import SwiftUI
 import CoreGraphics
 
-struct IterateColorDictionary: Sequence, IteratorProtocol {
-    var colorsDict = [[Color]](repeating: [Color.red, Color.blue, Color.yellow, Color.green, Color.orange, Color.white], count: 4).flatMap{$0}
-    var start: Int
+struct DraggableRect : View {
+    var rowId: Int
+    var colId: Int
     
-    mutating func next() -> Color? {
-        if start == 0 {
-            return Color.black
-        }
-        else {
-            defer {
-                start -= 1
-            }
-            return colorsDict[start]
-        }
+    var colorsDict = ([[Color]](repeating: [Color.red, Color.blue, Color.yellow, Color.green, Color.orange, Color.purple], count: 4).flatMap{$0} + [Color.clear]).shuffled()
+    
+    @GestureState
+    var position = CGSize.zero
+    
+    var body : some View {
+        RoundedRectangle(cornerRadius: 20.0)
+            .fill(colorsDict[rowId * 5 + colId])
+            .animation(.spring())
+            .offset(x: self.position.width, y: self.position.height)
+            .gesture(
+                DragGesture()
+                    .updating($position, body: { value, state, transaction in
+                                        state = value.translation
+                    })
+            )
     }
-    
-    
 }
 
 struct GameView: View {
     let grid = Grid.create()
     
     var sideLength: CGFloat = 60.0
-    var colorsDict = ([[Color]](repeating: [Color.red, Color.blue, Color.yellow, Color.green, Color.orange, Color.purple], count: 4).flatMap{$0} + [Color.clear]).shuffled()
     
     enum SwipeHorizontalDirection: String {
         case left, right, none
@@ -39,11 +42,6 @@ struct GameView: View {
     
     @State
     private var didShuffle: Bool = true
-    @State
-    var swipeHorizontalDirection: SwipeHorizontalDirection = .none {
-        didSet { print(swipeHorizontalDirection) } }
-    @State
-    var draggedOffset = CGSize.zero
     
     var body: some View {
         VStack {
@@ -52,20 +50,8 @@ struct GameView: View {
                         HStack{
                             ForEach(0..<5) {colIndex in
                                 VStack{
-                                    RoundedRectangle(cornerRadius: 20.0)
-                                        .fill(colorsDict[rowIndex * 5 + colIndex])
-                                        .frame(width: sideLength, height: sideLength)
-                                        // drag animation
-                                        .animation(.spring())
-                                        .offset(x: self.draggedOffset.width, y: self.draggedOffset.height)
-                                        .gesture(DragGesture()
-                                            .onChanged { value in
-                                                self.draggedOffset = value.translation
-                                            }
-                                            .onEnded { value in
-                                                self.draggedOffset = CGSize.zero
-                                            }
-                                        )
+                                        DraggableRect(rowId: rowIndex, colId: colIndex)
+                                            .frame(width: sideLength, height: sideLength)
                                 }
                             }
                         }
